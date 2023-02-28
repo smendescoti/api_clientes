76dfa623a9568f74b76b6423d8b061d7422f3754
smendescoti <sergio.coti@gmail.com>
2023-02-27 21:45:31 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/repositories/IClienteRepository.java
@@ -0,0 +1,11 @@
+package br.com.cotiinformatica.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cotiinformatica.entities.Cliente;
+
+@Repository
+public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
